66fb6b86e30e18aa1f5293e4db3f0308b86cf8a6
nate <70911747+nathanallen242@users.noreply.github.com>
2024-01-08 15:09:15 -0500

    availability near done; need to add confirmation

--- screens/appointment/Availability.tsx
+++ screens/appointment/Availability.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { SafeAreaView, Text, View, Image, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import { SafeAreaView, Text, View, Image, StyleSheet, FlatList, TouchableOpacity, Animated, Easing, ScrollView } from 'react-native';
 import { AppointmentContext } from '../../contexts/AppointmentContext';
 import { ref, onValue } from 'firebase/database';
 import { FIREBASE_DB } from '../../config/FireBase';
@@ -10,7 +10,10 @@ const Availability: React.FC = () => {
   const { appointmentDetails } = useContext(AppointmentContext);
   const [barber, setBarber] = useState<BarberData | null>(null);
   const [selectedDay, setSelectedDay] = useState<string | null>(null);
+  const [opacity, setOpacity] = useState(new Animated.Value(0));
   const selectedBarber = appointmentDetails?.employee_id;
+
+  const DAYS_MAP = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 
   useEffect(() => {
     if (selectedBarber) {
@@ -23,6 +26,26 @@ const Availability: React.FC = () => {
     }
   }, [selectedBarber]);
 
+
+  useEffect(() => {
+    if (selectedDay) {
+      Animated.timing(opacity, {
+        toValue: 1,
+        duration: 500,
+        easing: Easing.linear,
+        useNativeDriver: true,
+      }).start();
+    } else {
+      Animated.timing(opacity, {
+        toValue: 0,
+        duration: 500,
+        easing: Easing.linear,
+        useNativeDriver: true,
+      }).start();
+    }
+  }, [selectedDay]);
+
+
   const getDays = () => {
     const days = [];
     const today = new Date();
@@ -34,45 +57,57 @@ const Availability: React.FC = () => {
     return days;
   };
 
-
+  
   const renderDayItem = ({ item: dayId }: { item: string }) => {
-   const dayOfWeek = ((new Date(dayId).getDay() + 6) % 7);
-   const dayNumber = new Date(dayId).getDate();
-   const dayAbbreviation = new Date(dayId).toLocaleDateString(undefined, { weekday: 'short' });
-   return (
-   <TouchableOpacity style={styles.dayBlock} onPress={() => setSelectedDay(dayOfWeek.toString())}>
-     <Text style={styles.dayNumber}>{dayNumber}</Text>
-     <Text style={styles.dayAbbreviation}>{dayAbbreviation}</Text>
-   </TouchableOpacity>
-   );
-  };
+    const dayOfWeek = ((new Date(dayId).getDay() + 6) % 7);
+    const dayNumber = new Date(dayId).getDate();
+    const dayAbbreviation = DAYS_MAP[dayOfWeek];
+    return (
+     <TouchableOpacity 
+       style={[styles.dayBlock, selectedDay === dayOfWeek.toString() && styles.selectedDay]} 
+       onPress={() => setSelectedDay(dayOfWeek.toString())}
+     >
+       <Text style={styles.dayNumber}>{dayNumber}</Text>
+       <Text style={styles.dayAbbreviation}>{dayAbbreviation}</Text>
+     </TouchableOpacity>
+    );
+   };
 
 
   const renderTimeBlocks = () => {
-   if (!selectedDay || !barber?.availability[selectedDay]) {
-     console.log("selectedDay or barber.availability[selectedDay] is not defined");
-     return null;
-   }
-  
-   const availability = barber.availability[selectedDay];
-   const fromHour = parseInt(availability.from.split(':')[0], 10);
-   const toHour = parseInt(availability.to.split(':')[0], 10);
-  
-   return (
-     <FlatList
-       data={Array.from({ length: toHour - fromHour }, (_, i) => i + fromHour)}
-       renderItem={({ item: hour }) => (
-         <TouchableOpacity style={styles.timeBlock}>
-           <Text>{hour}:00</Text>
-         </TouchableOpacity>
-       )}
-       keyExtractor={(item) => item.toString()}
-     />
-   );
+    if (!selectedDay || !barber?.availability[selectedDay]) {
+      console.log("selectedDay or barber.availability[selectedDay] is not defined");
+      return null;
+    }
+ 
+    const availability = barber.availability[selectedDay];
+    const fromHour = parseInt(availability.from.split(':')[0], 10);
+    const toHour = parseInt(availability.to.split(':')[0], 10);
+ 
+    return (
+      <Animated.View style={{...styles.timeContainer, opacity}}>
+       <FlatList
+         data={Array.from({ length: toHour - fromHour }, (_, i) => i + fromHour)}
+         renderItem={({ item: hour }) => {
+           const period = hour >= 12 ? 'PM' : 'AM';
+           const formattedHour = hour % 12 || 12;
+           return (
+             <TouchableOpacity 
+               style={[styles.timeBlock, selectedDay && selectedDay === hour.toString() ? styles.selectedTimeBlock : {}]}
+             >
+               <Text>{formattedHour}:00 {period}</Text>
+             </TouchableOpacity>
+           );
+         }}
+         keyExtractor={(item) => item.toString()}
+       />
+      </Animated.View>
+      );
   };
 
   return (
-    <SafeAreaView>
+    <SafeAreaView style = {{flex: 1}}>
+      <ScrollView style = {{flex: 1}}>
       <View style={styles.container}>
         <Text style={styles.title}>Step 3: Book your Slot</Text>
         {barber && (
@@ -87,6 +122,7 @@ const Availability: React.FC = () => {
               style={{ alignSelf: 'center', marginTop: 5 }}
               onChange={() => {}}
             />
+            <Text style={styles.dayHeader}>Day</Text>
             <FlatList
               data={getDays()}
               horizontal
@@ -95,15 +131,32 @@ const Availability: React.FC = () => {
               style={styles.flatList}
               showsHorizontalScrollIndicator={false}
             />
+            <Text style={styles.availabilityHeader}>Availability</Text>
             {renderTimeBlocks()}
           </>
         )}
       </View>
+      </ScrollView>
     </SafeAreaView>
-  );
+   );   
 };
 
 const styles = StyleSheet.create({
+  availabilityHeader: {
+    fontSize: 23,
+    fontWeight: 'bold',
+    marginTop: 35,
+    marginLeft: 30,
+    marginBottom: 20,
+    alignSelf: 'flex-start'
+  },
+  dayHeader: {
+    fontSize: 23,
+    fontWeight: 'bold',
+    marginTop: 35,
+    marginLeft: 30,
+    alignSelf: 'flex-start'
+  },
   flatList: {
     margin: 30,
   },
@@ -125,32 +178,42 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 18,
   },
-  timeBlock: {
-    width: 50,
-    height: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 25,
-    backgroundColor: '#ccc',
-    marginVertical: 5,
-  },
   dayBlock: {
     padding: 20,
     marginHorizontal: 10,
     backgroundColor: '#eee',
     borderRadius: 10,
-    width: 63,
+    width: 65,
     height: 60,
     justifyContent: 'center',
     alignItems: 'center',
    },
-   dayNumber: {
+  dayNumber: {
     fontSize: 20,
     fontWeight: 'bold',
    },
-   dayAbbreviation: {
+  dayAbbreviation: {
     fontSize: 14,
    },
+  timeContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+  },
+  timeBlock: {
+    width: 100, // Increase width
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    backgroundColor: '#ccc',
+    marginVertical: 5,
+    marginLeft: 25,
+    marginHorizontal: 10,
+  },
+  selectedTimeBlock: {
+    backgroundColor: '#ff0', // Change to desired highlight color
+  },
 });
 
 export default Availability;
